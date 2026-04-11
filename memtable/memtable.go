bfa0a8c4b3a57fccc61d48508f05db2c59a1ae07
DecarbonizedGlucose <decglu001@gmail.com>
2026-04-12 00:51:03 +0800

    feature: add memtable basic functions implement

--- memtable/memtable.go
+++ memtable/memtable.go
@@ -5,6 +5,7 @@ import (
 	"sync"
 
 	"github.com/DecarbonizedGlucose/granite/comparer"
+	gerrors "github.com/DecarbonizedGlucose/granite/errors"
 )
 
 const (
@@ -101,4 +102,160 @@ func (m *MemTable) Len() int {
 	m.mu.RLock()
 	defer m.mu.RUnlock()
 	return m.n
+}
+
+// ==================== Searching and Writing ====================
+
+// find greater or equal ...
+// returns fist kv node that node.key >= key and if node.key == key
+// if there's no node.key >= key, return (0, false)
+// if prev == true, it needs to be called under RWLock
+func (m *MemTable) findGE(key []byte, prev bool) (int, bool) {
+	h := m.maxHeight - 1
+	node := 0
+	for {
+		next := m.nodeData[node+nNext+h]
+		cmpRes := 1
+		if next != 0 {
+			off := m.nodeData[next]
+			cmpRes = m.cmp.Compare(m.kvData[off:off+m.nodeData[next+nKey]], key)
+		}
+		if cmpRes < 0 {
+			node = next // this key is too little
+		} else {
+			if prev {
+				m.prevNode[h] = node
+			} else if cmpRes == 0 {
+				return next, true
+			}
+			if h == 0 {
+				return next, cmpRes == 0
+			}
+			h--
+		}
+	}
+}
+
+// Put sets value for "key". It overwrites previous values.
+func (m *MemTable) Put(key, value []byte) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if node, exact := m.findGE(key, true); exact {
+		// for being called as api
+		// exact will NOT be true when memtable used as db component
+		kvlen := len(m.kvData)
+		m.kvData = append(m.kvData, key...)
+		m.kvData = append(m.kvData, value...)
+		m.nodeData[node] = kvlen
+		vlo := m.nodeData[node+nVal]
+		m.nodeData[node+nVal] = len(value)
+		m.kvSize += len(value) - vlo
+		return nil
+	}
+
+	// normal put using internal key
+
+	newh := m.genRandomHeight()
+	if newh > m.maxHeight {
+		for i := m.maxHeight; i < newh; i++ {
+			m.prevNode[i] = 0 // initial
+		}
+		m.maxHeight = newh
+	}
+
+	kvlen := len(m.kvData)
+	m.kvData = append(m.kvData, key...)
+	m.kvData = append(m.kvData, value...)
+
+	newNode := len(m.nodeData)
+	m.nodeData = append(m.nodeData, kvlen, len(key), len(value), newh)
+	for i, nodeo := range m.prevNode[:newh] {
+		nexto := nodeo + nNext + i
+		// set node->next at each level
+		m.nodeData = append(m.nodeData, m.nodeData[nexto])
+		// set node->prev->next = node
+		m.nodeData[nexto] = newNode
+	}
+
+	m.kvSize += len(key) + len(value)
+	m.n++
+
+	return nil
+}
+
+// Contains returns if "key" is in the memtable.
+func (m *MemTable) Contains(key []byte) bool {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+	_, exact := m.findGE(key, false)
+	return exact
+}
+
+// Find finds {k, v} >= "key".
+// If wanted {k, v} not found, it returns with ErrNotFound.
+func (m *MemTable) Find(key []byte) (rkey, value []byte, err error) {
+	m.mu.RLock()
+	defer m.mu.RUnlock()
+
+	node, _ := m.findGE(key, false)
+	if node != 0 {
+		ko := m.nodeData[node]
+		vo := ko + m.nodeData[node+nKey]
+		rkey = m.kvData[ko:vo]
+		value = m.kvData[vo : vo+m.nodeData[node+nVal]]
+	} else {
+		err = gerrors.ErrNotFound
+	}
+	return
+}
+
+// Get gets the value for "key".
+// Return with ErrNotFound if "key" does not exist.
+func (m *MemTable) Get(key []byte) (value []byte, err error) {
+	m.mu.Lock()
+	defer m.mu.RUnlock()
+
+	node, exact := m.findGE(key, false)
+	if exact {
+		vo := m.nodeData[node] + m.nodeData[node+nKey] // value offset
+		value = m.kvData[vo : vo+m.nodeData[node+nVal]]
+	} else {
+		err = gerrors.ErrNotFound
+	}
+	return
+}
+
+// Delete deletes "key" and its value.
+// Return with ErrNotFound if "key" does not exist.
+func (m *MemTable) Delete(key []byte) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	toDelNode, exact := m.findGE(key, true)
+	if exact == false {
+		return gerrors.ErrNotFound
+	}
+
+	nh := m.nodeData[toDelNode+nHeight]
+	for i, nodeo := range m.prevNode[:nh] {
+		nexto := nodeo + nNext + i
+		m.nodeData[nexto] = m.nodeData[m.nodeData[nexto]+nNext+i] // node->next = node->next->next
+	}
+
+	m.kvSize -= m.nodeData[toDelNode+nKey] + m.nodeData[toDelNode+nVal]
+	m.n--
+
+	return nil
+}
+
+// ==================== Utils ====================
+
+func (m *MemTable) genRandomHeight() int {
+	const branching = 4
+	h := 1
+	for h < slMaxHeight && m.rnd.Int()%branching == 0 {
+		h++
+	}
+	return h
 }
