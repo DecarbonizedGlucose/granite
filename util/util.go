589be7f8deb30031d7820e40fc32e854f3ff57c1
DecarbonizedGlucose <decglu001@gmail.com>
2026-05-24 20:13:25 +0800

    feature: define a base releaser interface and add its empty implementation

--- util/util.go
+++ util/util.go
@@ -1,5 +1,13 @@
 package util
 
+type Releaser interface {
+	Close()
+}
+
+type NoopReleaser struct{}
+
+func (NoopReleaser) Close() {}
+
 func EnsureBuffer(buf []byte, c int) []byte {
 	if cap(buf) < c {
 		return make([]byte, c)
