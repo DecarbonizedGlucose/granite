c735719f6a311f31420e1d3944f994bd62650c14
DecarbonizedGlucose <decglu001@gmail.com>
2026-05-24 21:56:10 +0800

    feat(ache): add missing murmur32 hash function

--- cache/cache.go
+++ cache/cache.go
@@ -722,3 +722,48 @@ func (h *mHead) enumerateNodesByNS(ns uint64) []*Node {
 	return nodes
 }
 
+// murmur32 implements MurmurHash2 32-bit hash function.
+func murmur32(ns, key uint64, seed uint32) uint32 {
+	const (
+		m = uint32(0x5bd1e995)
+		r = 24
+	)
+
+	k1 := uint32(ns >> 32)
+	k2 := uint32(ns)
+	k3 := uint32(key >> 32)
+	k4 := uint32(key)
+
+	k1 *= m
+	k1 ^= k1 >> r
+	k1 *= m
+
+	k2 *= m
+	k2 ^= k2 >> r
+	k2 *= m
+
+	k3 *= m
+	k3 ^= k3 >> r
+	k3 *= m
+
+	k4 *= m
+	k4 ^= k4 >> r
+	k4 *= m
+
+	h := seed
+
+	h *= m
+	h ^= k1
+	h *= m
+	h ^= k2
+	h *= m
+	h ^= k3
+	h *= m
+	h ^= k4
+
+	h ^= h >> 13
+	h *= m
+	h ^= h >> 15
+
+	return h
+}
