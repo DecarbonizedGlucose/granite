18ec11ad3fde2e630f14cbe75d941165b6e9b42d
DecarbonizedGlucose <decglu001@gmail.com>
2026-05-31 18:18:55 +0800

    feat(option): add compaction controlling options

--- opt/options.go
+++ opt/options.go
@@ -1,6 +1,8 @@
 package opt
 
 import (
+	"math"
+
 	"github.com/DecarbonizedGlucose/granite/comparer"
 	"github.com/DecarbonizedGlucose/granite/filter"
 )
@@ -21,22 +23,54 @@ const (
 )
 
 var (
-	DefaultBlockSize       = 4 * KiB
-	DefaultBlockRestartGap = 16
-	DefaultCompressionType = SnappyCompression
-	DefaultFilterBaseLg    = 11
-	DefaultWriteBufferSize = 4 * MiB
+	DefaultBlockSize                     = 4 * KiB
+	DefaultBlockRestartGap               = 16
+	DefaultCompactionExpandLimitFactor   = 25
+	DefaultCompactionGPOverlapsFactor    = 10
+	DefaultCompactionSourceLimitFactor   = 1
+	DefaultCompactionTableSize           = 2 * MiB
+	DefaultCompactionTableSizeMultiplier = 1.0
+	DefaultCompactionTotalSize           = 10 * MiB
+	DefaultCompactionTotalSizeMultiplier = 10.0
+	DefaultCompressionType               = SnappyCompression
+	DefaultFilterBaseLg                  = 11
+	DefaultWriteBufferSize               = 4 * MiB
 )
 
 type Options struct {
+	// AltFilters defines one or more 'alternative filters'.
 	AltFilters      []filter.FilterPolicy
 	BlockRestartGap int
 	BlockSize       int
-	Comparer        comparer.Comparer
-	Compression     CompressionType
-	Filter          filter.FilterPolicy
-	FilterBaseLg    int
-	WriteBufferSize int
+	// CompactionExpandLimitFactor limits compaction size after expanded.
+	CompactionExpandLimitFactor int
+	// CompactionGPOverlapsFactor limits overlaps in grandparent (Level + 2) that a
+	// single 'sorted table' generates.
+	CompactionGPOverlapsFactor int
+	// CompactionSourceLimitFactor limits compaction source size.
+	// This doesn't apply to level-0.
+	CompactionSourceLimitFactor int
+	// CompactionTableSize limits size of 'sorted table' that compaction generates.
+	CompactionTableSize int
+	// CompactionTableSizeMultiplier defines multiplier for CompactionTableSize.
+	CompactionTableSizeMultiplier float64
+	// CompactionTableSizeMultiplierPerLevel defines per-level
+	// multiplier for CompactionTableSize.
+	// Use zero to skip a level.
+	CompactionTableSizeMultiplierPerLevel []float64
+	// CompactionTotalSize limits total size of 'sorted table' for each level.
+	CompactionTotalSize int
+	// CompactionTotalSizeMultiplier defines multiplier for CompactionTotalSize.
+	CompactionTotalSizeMultiplier float64
+	// CompactionTotalSizeMultiplierPerLevel defines per-level multiplier for
+	// CompactionTotalSize.
+	// Use zero to skip a level.
+	CompactionTotalSizeMultiplierPerLevel []float64
+	Comparer                              comparer.Comparer
+	Compression                           CompressionType
+	Filter                                filter.FilterPolicy
+	FilterBaseLg                          int
+	WriteBufferSize                       int
 }
 
 func (o *Options) GetAltFilters() []filter.FilterPolicy {
@@ -58,6 +92,72 @@ func (o *Options) GetBlockSize() int {
 		return DefaultBlockSize
 	}
 	return o.BlockSize
+}
+
+func (o *Options) GetCompactionExpandLimit(level int) int {
+	factor := DefaultCompactionExpandLimitFactor
+	if o != nil && o.CompactionExpandLimitFactor > 0 {
+		factor = o.CompactionExpandLimitFactor
+	}
+	return o.GetCompactionTableSize(level+1) * factor
+}
+
+func (o *Options) GetCompactionGPOverlaps(level int) int {
+	factor := DefaultCompactionGPOverlapsFactor
+	if o != nil && o.CompactionGPOverlapsFactor > 0 {
+		factor = o.CompactionGPOverlapsFactor
+	}
+	return o.GetCompactionTableSize(level+2) * factor
+}
+
+func (o *Options) GetCompactionSourceLimit(level int) int {
+	factor := DefaultCompactionSourceLimitFactor
+	if o != nil && o.CompactionSourceLimitFactor > 0 {
+		factor = o.CompactionSourceLimitFactor
+	}
+	return o.GetCompactionTableSize(level+1) * factor
+}
+
+func (o *Options) GetCompactionTableSize(level int) int {
+	var (
+		base = DefaultCompactionTableSize
+		mult float64
+	)
+	if o != nil {
+		if o.CompactionTableSize > 0 {
+			base = o.CompactionTableSize
+		}
+		if level < len(o.CompactionTableSizeMultiplierPerLevel) && o.CompactionTableSizeMultiplierPerLevel[level] > 0 {
+			mult = o.CompactionTableSizeMultiplierPerLevel[level]
+		} else if o.CompactionTableSizeMultiplier > 0 {
+			mult = math.Pow(o.CompactionTableSizeMultiplier, float64(level))
+		}
+	}
+	if mult == 0 {
+		mult = math.Pow(DefaultCompactionTableSizeMultiplier, float64(level))
+	}
+	return int(float64(base) * mult)
+}
+
+func (o *Options) GetCompactionTotalSize(level int) int64 {
+	var (
+		base = DefaultCompactionTotalSize
+		mult float64
+	)
+	if o != nil {
+		if o.CompactionTotalSize > 0 {
+			base = o.CompactionTotalSize
+		}
+		if level < len(o.CompactionTotalSizeMultiplierPerLevel) && o.CompactionTotalSizeMultiplierPerLevel[level] > 0 {
+			mult = o.CompactionTotalSizeMultiplierPerLevel[level]
+		} else if o.CompactionTotalSizeMultiplier > 0 {
+			mult = math.Pow(o.CompactionTotalSizeMultiplier, float64(level))
+		}
+	}
+	if mult == 0 {
+		mult = math.Pow(DefaultCompactionTotalSizeMultiplier, float64(level))
+	}
+	return int64(float64(base) * mult)
 }
 
 func (o *Options) GetComparer() comparer.Comparer {
--- options.go
+++ options.go
@@ -27,6 +27,8 @@ func (s *session) setOptions(o *opt.Options) {
 
 }
 
+const optCachedLevel = 7
+
 type cachedOptions struct {
 	*opt.Options
 
@@ -35,4 +37,55 @@ type cachedOptions struct {
 	compactionSourceLimit []int
 	compactionTableSize   []int
 	compactionTotalSize   []int64
+}
+
+func (co *cachedOptions) cache() {
+	co.compactionExpandLimit = make([]int, optCachedLevel)
+	co.compactionGPOverlaps = make([]int, optCachedLevel)
+	co.compactionSourceLimit = make([]int, optCachedLevel)
+	co.compactionTableSize = make([]int, optCachedLevel)
+	co.compactionTotalSize = make([]int64, optCachedLevel)
+
+	for l := 0; l < optCachedLevel; l++ {
+		co.compactionExpandLimit[l] = co.Options.GetCompactionExpandLimit(l)
+		co.compactionGPOverlaps[l] = co.Options.GetCompactionGPOverlaps(l)
+		co.compactionSourceLimit[l] = co.Options.GetCompactionSourceLimit(l)
+		co.compactionTableSize[l] = co.Options.GetCompactionTableSize(l)
+		co.compactionTotalSize[l] = co.Options.GetCompactionTotalSize(l)
+	}
+}
+
+func (co *cachedOptions) GetCompactionExpandLimit(level int) int {
+	if level < optCachedLevel {
+		return co.compactionExpandLimit[level]
+	}
+	return co.Options.GetCompactionExpandLimit(level)
+}
+
+func (co *cachedOptions) GetCompactionGPOverlaps(level int) int {
+	if level < optCachedLevel {
+		return co.compactionGPOverlaps[level]
+	}
+	return co.Options.GetCompactionGPOverlaps(level)
+}
+
+func (co *cachedOptions) GetCompactionSourceLimit(level int) int {
+	if level < optCachedLevel {
+		return co.compactionSourceLimit[level]
+	}
+	return co.Options.GetCompactionSourceLimit(level)
+}
+
+func (co *cachedOptions) GetCompactionTableSize(level int) int {
+	if level < optCachedLevel {
+		return co.compactionTableSize[level]
+	}
+	return co.Options.GetCompactionTableSize(level)
+}
+
+func (co *cachedOptions) GetCompactionTotalSize(level int) int64 {
+	if level < optCachedLevel {
+		return co.compactionTotalSize[level]
+	}
+	return co.Options.GetCompactionTotalSize(level)
 }
